6a82c53a02609a8bbb89871cdae06277b6dc5296
sphindle1 <will14709@gmail.com>
2018-05-31 01:24:54 -0700

    save point

--- app/src/main/java/com/example/william/a24/SingleMode.java
+++ app/src/main/java/com/example/william/a24/SingleMode.java
@@ -1,13 +1,16 @@
 package com.example.william.a24;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.os.CountDownTimer;
 import java.util.ArrayList;
 
 public class SingleMode extends AppCompatActivity {
@@ -23,6 +26,9 @@ public class SingleMode extends AppCompatActivity {
     String d;
     String curNum;
     char operator;
+    int count;
+    double score;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +39,47 @@ public class SingleMode extends AppCompatActivity {
         num3 = (Button) findViewById(R.id.button9);
         num4 = (Button) findViewById(R.id.button10);
         prev = num1; //arbitrary at this point
-        a = number();
-        b = number();
-        c = number();
-        d = number();
-        num1.setText(a);
-        num2.setText(b);
-        num3.setText(c);
-        num4.setText(d);
-        curNum = "";
-        operator = '?';
+        newNumbers();
+        score = 0;
+        final TextView timer = (TextView) findViewById(R.id.timer);
+        final TextView endText = new TextView(this);
+        final Button restart = new Button(this);
+        new CountDownTimer(120000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                timer.setText("Time: " + millisUntilFinished / 1000 + "s");
+                //here you can have your logic to set text to edittext
+            }
+
+            public void onFinish() {
+                ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.Layout);
+                layout.removeAllViews();
+                endText.setText("Score: " + score);
+                endText.setTextSize(36);
+                endText.setId(View.generateViewId());
+                restart.setId(2);
+                layout.addView(endText);
+                layout.addView(restart);
+                ConstraintSet constraintSet = new ConstraintSet();
+                constraintSet.clone(layout);
+                endText.setGravity(Gravity.CENTER);
+                restart.setText("Retry");
+                restart.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(SingleMode.this, SingleMode.class);
+                        startActivity(intent);
+                    }
+                });
+                constraintSet.connect(restart.getId(),ConstraintSet.TOP,endText.getId(),ConstraintSet.BOTTOM,0);
+                constraintSet.applyTo(layout));
+            }
+        }.start();
     }
     public void pickNum(View view) {
+        score -= 0.5;
+        newNumbers();
+    }
+    private void newNumbers() {
         num1.setVisibility(View.VISIBLE);
         num2.setVisibility(View.VISIBLE);
         num3.setVisibility(View.VISIBLE);
@@ -53,20 +88,21 @@ public class SingleMode extends AppCompatActivity {
         b = number();
         c = number();
         d = number();
+        while (Solution.solve(a, b, c, d).size() == 0) {
+            a = number();
+            b = number();
+            c = number();
+            d = number();
+        }
         num1.setText(a);
         num2.setText(b);
         num3.setText(c);
         num4.setText(d);
-    }
-    public void showSolution(View view) {
-        ArrayList<String> answers = Solution.solve(a, b, c, d);
-        String ans;
-        if (answers.size() == 0) {
-            ans = "impossible";
-        } else {
-            ans = answers.get(0);
-        }
-        sol.setText(ans);
+        TextView scoreBoard = (TextView) findViewById(R.id.scoreBoard);
+        scoreBoard.setText("Score: " + score);
+        count = 0;
+        curNum = "";
+        operator = '?';
     }
     private String number() {
         return "" + ((int)(Math.random()*13) + 1);
@@ -86,7 +122,7 @@ public class SingleMode extends AppCompatActivity {
             curNum = picked.getText().toString();
             prev = picked;
         } else {
-            if (operator == '/' && picked.getText().toString() == "0") {
+            if (operator == '/' && picked.getText().toString().equals("0")) {
                 curNum = "0";
                 prev = picked;
             } else {
@@ -96,6 +132,13 @@ public class SingleMode extends AppCompatActivity {
                 picked.setText(curNum);
                 prev.setVisibility(View.INVISIBLE);
                 prev = picked;
+                count++;
+                if (count == 3) {
+                    if (curNum.equals("24")) {
+                        score++;
+                        newNumbers();
+                    }
+                }
             }
         }
     }
